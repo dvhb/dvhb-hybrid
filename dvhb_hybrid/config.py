7bc3c1defd3d63c974b1057e5f28371237e30595
Alexander Malev <am@dvhb.ru>
2016-10-23 04:08:02 +0300

    db_to_settings

--- dvhb_hybrid/config.py
+++ dvhb_hybrid/config.py
@@ -31,3 +31,26 @@ def dirs(list_dir, base_dir):
     for i in list_dir:
         result.append(absdir(i, base_dir))
     return result
+
+
+def convert_to_djangodb(d, name, base_dir='/tmp'):
+    if d.get('database'):
+        db = {
+            k.upper(): v
+            for k, v in d.items()
+            if v}
+        db['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+        db['NAME'] = db.pop('DATABASE')
+    else:
+        return {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(base_dir, name + '.sqlite3'),
+        }
+    return db
+
+
+def db_to_settings(db_dict, base_dir):
+    return {
+        n: convert_to_djangodb(v, n, base_dir=base_dir)
+        for n, v in db_dict.items()
+    }
