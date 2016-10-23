f3dadf5b3d1d14403c68eefdaa218aa7d942f6b2
Alexander Malev <am@dvhb.ru>
2016-10-23 22:04:30 +0300

    Выделение RedisMixin

--- dvhb_hybrid/aviews.py
+++ dvhb_hybrid/aviews.py
@@ -6,6 +6,8 @@ import uuid
 from aiohttp import web
 from aiohttp_apiset.views import ApiSet
 
+from .redis import RedisMixin
+
 
 class JsonEncoder(json.JSONEncoder):
     ensure_ascii = False
@@ -32,54 +34,12 @@ dumper = functools.partial(
     json.dumps, ensure_ascii=False, cls=JsonEncoder)
 
 
-class BaseView(ApiSet):
-    default_response = 'json2'
+class BaseView(RedisMixin, ApiSet):
     limit_body = None
 
     @property
     def app(self):
         return self.request.app
-
-    @classmethod
-    def redis_key(cls, key, namespace=None):
-        namespace = namespace or cls.namespace
-        return 'vprs:{}:{}'.format(namespace.replace('/', ':'), key)
-
-    @classmethod
-    async def redis(cls, request, key, *, value=None, sadd=None, expire=None,
-                    default=None, namespace=None, smembers=False, srem=None,
-                    delete=False, connection=None):
-        key = cls.redis_key(key, namespace=namespace)
-
-        async def _redis(cache):
-            if value:
-                await cache.set(key, value)
-            elif delete:
-                await cache.delete(key)
-            elif sadd:
-                if isinstance(sadd, (list, tuple)):
-                    await cache.sadd(key, *sadd)
-                else:
-                    await cache.sadd(key, sadd)
-            elif smembers:
-                v = await cache.smembers(key)
-                return v or default
-            elif srem:
-                return await cache.srem(key, srem)
-            elif not expire:
-                return (await cache.get(key)) or default
-            if expire:
-                await cache.expire(key, int(expire))
-
-        if connection is not None:
-            return await _redis(connection)
-        else:
-            async with request.app.redis.get() as connection:
-                return await _redis(connection)
-
-    @classmethod
-    def response_json2(cls, data, **kwargs):
-        return web.json_response(data, dumps=dumper)
 
     def check_size_body(self, request, limit=None):
         limit = limit or self.limit_body
---
+++ dvhb_hybrid/redis.py
@@ -0,0 +1,42 @@
+class RedisMixin:
+    project_slug = None
+
+    @classmethod
+    def redis_key(cls, key, namespace=None):
+        namespace = namespace or cls.namespace
+        return '{}:{}:{}'.format(
+            cls.project_slug,
+            namespace.replace('/', ':'),
+            key)
+
+    @classmethod
+    async def redis(cls, request, key, *, value=None, sadd=None, expire=None,
+                    default=None, namespace=None, smembers=False, srem=None,
+                    delete=False, connection=None):
+        key = cls.redis_key(key, namespace=namespace)
+
+        async def _redis(cache):
+            if value:
+                await cache.set(key, value)
+            elif delete:
+                await cache.delete(key)
+            elif sadd:
+                if isinstance(sadd, (list, tuple)):
+                    await cache.sadd(key, *sadd)
+                else:
+                    await cache.sadd(key, sadd)
+            elif smembers:
+                v = await cache.smembers(key)
+                return v or default
+            elif srem:
+                return await cache.srem(key, srem)
+            elif not expire:
+                return (await cache.get(key)) or default
+            if expire:
+                await cache.expire(key, int(expire))
+
+        if connection is not None:
+            return await _redis(connection)
+        else:
+            async with request.app.redis.get() as connection:
+                return await _redis(connection)
