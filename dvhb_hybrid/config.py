638b6998432a037cbd8e2d1738a55903e05e1a16
abogushov <abogushov@gmail.com>
2016-10-25 19:34:17 +0500

    parametrize user prefix in config

--- dvhb_hybrid/config.py
+++ dvhb_hybrid/config.py
@@ -4,18 +4,18 @@ import invoke
 import yaml
 
 
-def load_conf(overrides=None, *, base_path, env_path, env_prefix, system_prefix):
+def load_conf(overrides=None, *, base_path, env_path, env_prefix, user_prefix, system_prefix):
     with open(base_path) as f:
         base = yaml.load(f)
     path = os.environ.get(env_path)
-    if not overrides and path:
+    if not overrides and path and os.path.isfile(path):
         with open(path) as f:
             overrides = yaml.load(f)
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(base_path), '..'))
     return invoke.Config(
-        defaults=base, overrides=overrides,
+        defaults=base,
+        overrides=overrides,
         system_prefix=system_prefix,
-        user_prefix=os.path.join(base_dir, 'conf'),
+        user_prefix=user_prefix,
         env_prefix=env_prefix,
     )
 
