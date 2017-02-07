a493368bf7b8d74026db104c610a4617578710f7
Alexander Bogushov <abogushov@gmail.com>
2017-02-07 18:35:19 +0500

    Release version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dvhb-hybrid',
-    version='0.0.3',
+    version='0.0.4',
     description='',
     author='Malev A',
     author_email='am@dvhb.ru',
