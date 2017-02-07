c72c9358afbdecafd27a05aa6944454bf97baf49
Alexander Bogushov <abogushov@gmail.com>
2017-02-07 14:25:25 +0500

    get_route

--- dvhb_hybrid/tests.py
+++ dvhb_hybrid/tests.py
@@ -7,8 +7,13 @@ class BaseTestApi:
 
     def __init__(self, client, user):
         self.client = client
+        self.app = client.server.app
         self.user = user
         self.headers = {'content-type': 'application/json'}
+
+    def get_route(self, name):
+        name = name if name.startswith(self.app.name) else '{}.{}'.format(self.app.name, name)
+        return self.app.router[name]
 
     @staticmethod
     async def check_status(result, response=HTTPOk):
