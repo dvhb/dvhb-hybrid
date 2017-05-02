4c693b097a04f4d644a64a2e03adc1cb34fe5468
abogushov <abogushov@gmail.com>
2017-05-02 10:13:34 +0500

    Files app default config

--- dvhb_hybrid/files/__init__.py
+++ dvhb_hybrid/files/__init__.py
@@ -3,3 +3,5 @@ from aiohttp_apiset.swagger.operations import OperationIdMapping
 from . import amodels, image  # noqa
 
 opid = OperationIdMapping(image)
+
+default_app_config = 'dvhb_hybrid.files.apps.FilesConfig'
