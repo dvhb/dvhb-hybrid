b97ade0b459c00f2701dc6cc731f60de6a8c4e45
Alexander Bogushov <abogushov@gmail.com>
2017-05-10 15:56:46 +0500

    Recaptcha decorator.

---
+++ dvhb_hybrid/auth.py
@@ -0,0 +1,46 @@
+import functools
+
+import aiohttp
+import aiohttp.web
+
+from .exceptions import HTTPNotAcceptable
+
+
+def recaptcha(arg):
+    """
+    Decorator to verify recaptcha response.
+    
+    Required properties in "recaptcha" config section:
+        url - url to send request.
+        active - true if recaptcha verification is active.
+        secret - code to access api.
+    """
+    def with_arg(view):
+        @functools.wraps(view)
+        async def wrapper(**kwargs):
+            request = kwargs['request']
+            c = request.app.config.recaptcha
+            response = None
+            for v in kwargs.values():
+                if isinstance(v, dict) and 'g-recaptcha-response' in v:
+                    response = v.pop('g-recaptcha-response')
+                    break
+
+            if not c.active:
+                return await view(**kwargs)
+
+            if response:
+                async with aiohttp.ClientSession() as client:
+                    async with client.post(c.url, data={'secret': c.secret, 'response': response}) as r:
+                        if r.status == aiohttp.web.HTTPOk.status_code:
+                            data = await r.json()
+                            if data['success']:
+                                return await view(**kwargs)
+
+            raise HTTPNotAcceptable(reason='Wrong recaptcha')
+
+        return wrapper
+
+    if not callable(arg):
+        return with_arg
+    return with_arg(arg)
