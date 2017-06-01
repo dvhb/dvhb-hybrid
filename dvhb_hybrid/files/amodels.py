f8db68b72bcffcf1039822763664f736b10a4825
Alexander Malev <am@dvhb.ru>
2017-06-01 12:12:32 +0300

    image.from_url

--- dvhb_hybrid/files/amodels.py
+++ dvhb_hybrid/files/amodels.py
@@ -1,8 +1,13 @@
+import mimetypes
+from io import BytesIO
+
+from aiohttp import client
 from sqlalchemy import table, column
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 
 from ..amodels import Model
 from .. import utils
+from .storages import image_storage
 
 
 class Image(Model):
@@ -22,3 +27,36 @@ class Image(Model):
     def set_defaults(cls, data: dict):
         data.setdefault('meta', {})
         data['updated_at'] = utils.now()
+
+    @classmethod
+    async def from_url(cls, url, *, user, connection=None):
+        async with client.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status != 200:
+                    return
+                l = response.content_length
+                if not l or l > 2 ** 23:
+                    return
+                content_type = response.content_type
+                if not content_type.startswith('image'):
+                    return
+                content = BytesIO(await response.read())
+                filename = response.url.name
+        exts = mimetypes.guess_all_extensions(content_type)
+        for ext in exts:
+            if filename.endswith(ext):
+                break
+        else:
+            if exts:
+                filename += exts[-1]
+        name = await cls.app.loop.run_in_executor(
+            None, image_storage.save, filename, content)
+        image_uuid = image_storage.uuid(name)
+        return await cls.create(
+            uuid=image_uuid,
+            image=name,
+            mime_type=content_type,
+            created_at=utils.now(),
+            author_id=user.pk,
+            connection=connection
+        )
