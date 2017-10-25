38b78ea49caefba49a4557450eb584df32050df4
Denis Shatov <radiophysicist@users.noreply.github.com>
2017-10-25 08:16:34 +0700

    Email templates v2 (#2)
    * Add email templates
    * Add email_templates.get_template_translation()
    * Add DB template search into Mailer.send()
    * Temporarily disable running of tests for email templates (due to nonavailability of test DB)

--- dvhb_hybrid/mailer/base.py
+++ dvhb_hybrid/mailer/base.py
@@ -6,6 +6,7 @@ from aioworkers.core.context import Context
 from aioworkers.utils import module_path
 from aioworkers.worker.base import Worker
 
+from dvhb_hybrid.amodels import method_connect_once
 from .. import utils
 from .template import load_all
 
@@ -112,13 +113,36 @@ class BaseMailer(Worker):
             loop=self.app.loop, conf=self.conf)
         return connection
 
+    @method_connect_once
+    async def get_template_translation(self, template_name, lang_code, fallback_lang_code='en', connection=None):
+        """
+        Requests translation of the email template with name given to the language specified.
+        If there is no translation to the requested language tries to find fallback one.
+
+        On success returns translation in the form {'subject': TEMPLATE_SUBJECT, 'body': TEMPLATE_BODY}
+        """
+
+        # Try to find template with name given
+        template = await self.app.models.email_template.get_by_name(template_name, connection=connection)
+        if template is None:
+            return
+        # Try to find its translation to specifed language
+        translation = await template.get_translation(lang_code, connection=connection)
+        # Try to find fallback translation if necessary
+        if translation is None:
+            translation = await template.get_translation(fallback_lang_code, connection=connection)
+        if translation:
+            return translation.as_dict()
+
     async def send(self, mail_to, subject=None, body=None, *,
                    context=None, connection=None, template=None,
-                   attachments=None, save=True):
+                   attachments=None, save=True, lang_code='en', fallback_lang_code='en'):
         if template:
-            tmpl = self.templates[template]
-            subject = tmpl['subject']
-            body = tmpl['body']
+            tr = await self.get_template_translation(template, lang_code, fallback_lang_code)
+            if not tr:
+                raise KeyError("No template named '{}' in DB".format(template))
+            subject = tr['subject']
+            body = tr['body']
 
         elif not subject or not body:
             raise ValueError()
