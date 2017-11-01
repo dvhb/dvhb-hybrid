052fb394cd42c547d205783dae3a1a242d5c8aec
Denis Shatov <dsh@dvhb.ru>
2017-11-02 01:09:12 +0700

    Implement user picture type/size validation in admin

--- dvhb_hybrid/users/admin.py
+++ dvhb_hybrid/users/admin.py
@@ -1,9 +1,52 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from django.core.files.images import get_image_dimensions
+from django.core.files.uploadedfile import UploadedFile
+from django.forms import forms
 from django.utils.translation import ugettext_lazy as _
+
+from .models import AbstractUser
+
+
+class UserForm(UserChangeForm):
+    class Meta:
+        model = AbstractUser
+        fields = '__all__'
+
+    def clean_picture(self):
+        """
+        Implements validation of new user picture uploaded
+        """
+
+        picture = self.cleaned_data.get('picture')
+
+        # New picture has been uploaded
+        if picture and isinstance(picture, UploadedFile):
+            # Validate content type
+            main, sub = picture.content_type.split('/')
+            if not (main == 'image' and sub in ['jpeg', 'pjpeg', 'gif', 'png']):
+                raise forms.ValidationError(
+                    _('Please use a JPEG, GIF or PNG image.'))
+
+            w, h = get_image_dimensions(picture)
+
+            # Validate picture dimensions
+            max_width = max_height = 1024
+            if w > max_width or h > max_height:
+                raise forms.ValidationError(
+                    _('Please use an image that is %sx%s pixels or smaller.' % (max_width, max_height)))
+
+            # Validate file size
+            if len(picture) > (500 * 1024):
+                raise forms.ValidationError(
+                    _('User picture size may not exceed 500 kB.'))
+
+        return picture
 
 
 class BaseUserAdmin(DjangoUserAdmin):
+    form = UserForm
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'picture')}),
