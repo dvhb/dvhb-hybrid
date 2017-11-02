6cd1679f23e0461435a54d2b38d9fb0dc0015520
Denis Shatov <dsh@dvhb.ru>
2017-11-02 17:03:58 +0700

    Disable changing of user picture in admin

--- dvhb_hybrid/users/admin.py
+++ dvhb_hybrid/users/admin.py
@@ -49,7 +49,7 @@ class BaseUserAdmin(DjangoUserAdmin):
     form = UserForm
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'picture')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
@@ -59,7 +59,7 @@ class BaseUserAdmin(DjangoUserAdmin):
             'classes': ('wide',),
             'fields': ('email', 'password1', 'password2')}
          ),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'picture')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
     )
 
     list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
