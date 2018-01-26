4ab9d0149e619e6b88a1757af58476ade65a6e56
Denis Shatov <dsh@dvhb.ru>
2018-01-26 19:04:12 +0700

    Fix deprecated import

--- dvhb_hybrid/user_action_log/base_admin.py
+++ dvhb_hybrid/user_action_log/base_admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.html import escape
 from .enums import UserActionLogEntrySubType
 
