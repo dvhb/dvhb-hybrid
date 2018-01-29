45b88b49e7990529b574749d899f5705c3182ee7
Ross Nomann <rossnomann@protonmail.com>
2018-01-29 17:01:27 +0300

    Extract amodels decorators

---
+++ dvhb_hybrid/amodels/decorators.py
@@ -0,0 +1,43 @@
+import functools
+
+from ..utils import get_app_from_parameters
+
+
+def method_connect_once(arg):
+    def with_arg(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            if kwargs.get('connection') is None:
+                app = get_app_from_parameters(*args, **kwargs)
+                async with app['db'].acquire() as connection:
+                    kwargs['connection'] = connection
+                    return await func(*args, **kwargs)
+            else:
+                return await func(*args, **kwargs)
+        return wrapper
+
+    if not callable(arg):
+        return with_arg
+    return with_arg(arg)
+
+
+def method_redis_once(arg):
+    redis = 'redis'
+
+    def with_arg(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            if kwargs.get(redis) is None:
+                app = get_app_from_parameters(*args, **kwargs)
+                async with app[redis].get() as connection:
+                    kwargs[redis] = connection
+                    return await func(*args, **kwargs)
+            else:
+                return await func(*args, **kwargs)
+        return wrapper
+
+    if not callable(arg):
+        redis = arg
+        return with_arg
+
+    return with_arg(arg)
