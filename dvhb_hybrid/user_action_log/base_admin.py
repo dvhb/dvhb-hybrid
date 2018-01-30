034ebaf555c0aead8f0ffa977c56fe65542879d7
Denis Shatov <dsh@dvhb.ru>
2018-01-30 21:20:35 +0700

    Fix nonworking model links in user action log

--- dvhb_hybrid/user_action_log/base_admin.py
+++ dvhb_hybrid/user_action_log/base_admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import reverse, NoReverseMatch
 from django.utils.html import escape
 from .enums import UserActionLogEntrySubType
+from django.utils.safestring import mark_safe
 
 
 class BaseUserActionLogEntryAdmin(admin.ModelAdmin):
@@ -47,15 +48,14 @@ class BaseUserActionLogEntryAdmin(admin.ModelAdmin):
         # Object has been removed
         if obj.subtype != UserActionLogEntrySubType.delete and obj.object_id is not None:
             try:
-                link = u'<a href="%s">%s</a>' % (
+                link = mark_safe('<a href="%s">%s</a>' % (
                     reverse('admin:%s_%s_change' % (ct.app_label, ct.model), args=[obj.object_id]),
                     escape(obj.object_repr),
-                )
+                ))
             except NoReverseMatch:
                 pass
 
         return link
 
-    object_link.allow_tags = True
     object_link.admin_order_field = 'object_repr'
     object_link.short_description = u'object'
