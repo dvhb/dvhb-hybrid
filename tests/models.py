cf0612482f1e5ffb31700b70741733e3faddb52a
Denis Shatov <dsh@dvhb.ru>
2018-04-06 12:05:08 +0700

    Add tests.User model. Move and fix DB fixtures

--- tests/models.py
+++ tests/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
-
+from dvhb_hybrid.users.models import AbstractUser
 
 ### For test_amodels
 
@@ -8,3 +8,7 @@ class Test(models.Model):
     id = models.IntegerField(primary_key=True)
     text = models.TextField()
     data = JSONField()
+
+
+class User(AbstractUser):
+    pass
