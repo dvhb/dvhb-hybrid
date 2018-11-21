3f462d6c64167032ad3de3912a0ebae94e3d34fe
Alexander Malev <am@dvhb.ru>
2018-11-22 02:43:00 +0300

    refactor test_template_translation

--- tests/mailer/test_mailer.py
+++ tests/mailer/test_mailer.py
@@ -4,26 +4,6 @@ import pytest
 from django.core import mail
 
 
-@pytest.fixture
-def config(config):
-    config.load_yaml("""
-    mailer:
-      autorun: true
-      persist: true
-      from_email: hybrid@dvhb.io
-      django_email_backend_params:
-        host: smtp.dvhb.io
-      cls: dvhb_hybrid.mailer.django.Mailer
-      django_email_backend: django.core.mail.backends.locmem.EmailBackend
-      templates_from_module: dvhb_hybrid
-    models:
-      mailer:
-        func: dvhb_hybrid.utils.import_class
-        args: [dvhb_hybrid.mailer.amodels.Message]
-    """)
-    return config
-
-
 @pytest.mark.django_db
 async def test_mailer(context, aiohttp_client):
     await aiohttp_client(context.app)
--- tests/mailer/test_template_translation.py
+++ tests/mailer/test_template_translation.py
@@ -2,14 +2,16 @@ import pytest
 
 
 @pytest.mark.django_db
-async def test_get_template_by_name(app, test_client):
+async def test_get_template_by_name(app, aiohttp_client):
+    await aiohttp_client(app)
     template_name = 'AccountActivation'
     template = await app.m.email_template.get_by_name(template_name)
     assert template is not None
 
 
 @pytest.mark.django_db
-async def test_get_translations(app, test_client):
+async def test_get_translations(app, aiohttp_client):
+    await aiohttp_client(app)
     template_name = 'AccountActivation'
     template = await app.m.email_template.get_by_name(template_name)
     translation = await template.get_translation('en')
