67b6c7f823be35e7446b98f5b1facb8185c578a7
Ross Nomann <rossnomann@protonmail.com>
2020-01-20 16:43:04 +0400

    Update operation ID mapping for image API

--- dvhb_hybrid/files/__init__.py
+++ dvhb_hybrid/files/__init__.py
@@ -2,6 +2,14 @@ from aiohttp_apiset.swagger.operations import OperationIdMapping
 
 from . import amodels, image  # noqa
 
-opid = OperationIdMapping(image)
+opid = OperationIdMapping(
+    image_upload=image.image_upload,
+    photo_handler_wh=image.photo_handler,
+    photo_handler_wh_2x=image.photo_handler,
+    photo_handler_origin_2x=image.photo_handler,
+    photo_handler_origin=image.photo_handler,
+    scale_to_width_or_height=image.scale_to_width_or_height,
+    scale_to_width_or_height_2x=image.scale_to_width_or_height
+)
 
 default_app_config = 'dvhb_hybrid.files.apps.FilesConfig'
