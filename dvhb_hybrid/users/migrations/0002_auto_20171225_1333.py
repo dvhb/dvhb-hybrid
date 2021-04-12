9b94401b517e9952793fcbb50dc4fedaa3888067
Ross Nomann <rossnomann@protonmail.com>
2021-04-12 15:28:37 +0300

    Fix migrations

--- dvhb_hybrid/users/migrations/0002_auto_20171225_1333.py
+++ dvhb_hybrid/users/migrations/0002_auto_20171225_1333.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dvhb_hybrid.users', '0001_initial'),
+        ('dvhb_hybrid_users', '0001_initial'),
     ]
 
     operations = [
